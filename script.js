01e5a5f5798afc081d4d6b638bab679f23acc274
Framedrop11 <mriguhoax.rajkhowa11@gmail.com>
2026-02-19 12:22:50 +0530

    added feature to read inputs

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("this is the project setup");
+function getInputs() {
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    
+    // return {
+    //     num1: parseFloat(num1),
+    //     num2: parseFloat(num2),
+    //     isValid: !isNaN(num1) && !isNaN(num2) && num1 !== '' && num2 !== ''
+    // };
+
+    return console.log(num1, num2);
+}
