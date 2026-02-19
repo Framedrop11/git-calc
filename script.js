c80284e357d80443f75e04302640bb2ef863323d
Framedrop11 <mriguhoax.rajkhowa11@gmail.com>
2026-02-19 12:25:12 +0530

    addition feature added

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ function getInputs() {
     const num1 = document.getElementById('num1').value;
     const num2 = document.getElementById('num2').value;
     
-    // return {
-    //     num1: parseFloat(num1),
-    //     num2: parseFloat(num2),
-    //     isValid: !isNaN(num1) && !isNaN(num2) && num1 !== '' && num2 !== ''
-    // };
-
-    return console.log(num1, num2);
+    return {
+        num1: parseFloat(num1),
+        num2: parseFloat(num2),
+        isValid: !isNaN(num1) && !isNaN(num2) && num1 !== '' && num2 !== ''
+    };
 }
+
+document.getElementById('add').addEventListener('click', function() {
+    const inputs = getInputs();
+    if (inputs.isValid) {
+        const result = inputs.num1 + inputs.num2;
+        console.log(`${inputs.num1} + ${inputs.num2} = ${result}`);
+    } else {
+        alert('Please enter valid numbers');
+    }
+});
+
